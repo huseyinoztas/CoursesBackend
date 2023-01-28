b36ea1b0917f00789304b255624e897f3b147e85
Huseyin Oztas <110281174+huseyinoztas@users.noreply.github.com>
2023-01-28 17:14:29 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
+
+
 const mongoose=require('mongoose')
 
 const connectDB=async ()=>{
-    mongoose.set('useNewUrlParser', true);
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useUnifiedTopology', true);
-
     const conn=await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDb Connected: ${conn.connection.host}`.magenta.bold);
+    console.log(`MongoDb Baglandi: ${conn.connection.host}`.magenta.bold);
 }
 
 module.exports=connectDB
