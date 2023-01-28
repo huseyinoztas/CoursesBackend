d793dba4fb061ce74959ed20cc7904dcc912a661
Huseyin Oztas <110281174+huseyinoztas@users.noreply.github.com>
2023-01-28 16:48:39 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
-import dotenv from 'dotenv';
+
 
 const mongoose=require('mongoose')
 
 const connectDB=async ()=>{
-    dotenv.config();
     const conn=await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDb Baglandi: ${conn.connection.host}`.magenta.bold);
